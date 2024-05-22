4878c72ae78ba616fd0866a9e2b24d77408be957
pablomartinsti <pablomartinsti@outlook.com>
2024-05-22 08:59:26 -0300

    commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+const checkUserId = (resquest,response ,next) =>{
+    const {id} = resquest.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({erro : "user not found"})
+    }
+
+
+    resquest.UserIndex = index
+    resquest.userId = id
+
+    next()
+}
+
+
+
+
+
+app.get('/users', (resquest, response) => {
+
+    return response.json(users)
+})
+
+app.post('/users', (resquest, response) => {
+    const { name, age } = resquest.body
+    console.log(uuid.v4())
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id',checkUserId, (resquest, response) => {
+    
+    const {name ,age} = resquest.body
+    const index = resquest.UserIndex
+    const id =resquest.userId
+
+    const updatedUser = {id ,name ,age}
+
+   
+
+    users [index] = updatedUser
+
+    return response.json(updatedUser)
+})
+
+app.delete('/users/:id',checkUserId, (resquest, response) => {
+    const index = resquest.UserIndex
+   
+
+   
+        users.splice(index, 1)
+
+    return response.status(204).json(users)
+})
+
+
+app.listen(port, () => {
+    console.log(`🚀🚀 server started on port ${port}`)
+})
